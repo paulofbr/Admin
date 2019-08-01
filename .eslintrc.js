51bb68ea12bdd4507eae16fbcd4a3d3fecfb9fc4
Paulo Rocha <PRocha@microio.local>
2019-08-01 23:01:22 +0100

    init

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true
+  },
+  parserOptions: {
+    parser: '@typescript-eslint/parser'
+  },
+  extends: [
+    '@nuxtjs',
+    'plugin:nuxt/recommended'
+  ],
+  // add your custom rules here
+  rules: {
+    '@typescript-eslint/no-unused-vars': 'error'
+  },
+  plugins:[
+   "@typescript-eslint"
+  ]
+}
---
+++ layouts/error.vue
@@ -0,0 +1,45 @@
+<template>
+  <v-app dark>
+    <h1 v-if="error.statusCode === 404">
+      {{ pageNotFound }}
+    </h1>
+    <h1 v-else>
+      {{ otherError }}
+    </h1>
+    <NuxtLink to="/">
+      Home page
+    </NuxtLink>
+  </v-app>
+</template>
+
+<script lang="ts">
+import { Component, Vue,Prop } from 'nuxt-property-decorator'
+
+interface Error{
+  statusCode:number
+}
+
+@Component({
+  layout:'empty'
+})
+export default class LogoComponent extends Vue {
+  
+  pageNotFound:string= '404 Not Found';
+  otherError:string='An error occurred';
+
+  @Prop({default:null})
+  error!:Error
+    
+  head () {
+    const title = this.error.statusCode === 404 ? this.pageNotFound : this.otherError
+    return {title}
+  }
+  
+}
+</script>
+
+<style scoped>
+h1 {
+  font-size: 20px;
+}
+</style>
